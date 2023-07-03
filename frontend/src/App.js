5fdce038eb787390c6d3c7220f3990788abc8742
AtanasoaieD <danatanasoaie17@gmail.com>
2023-07-03 19:49:19 +0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Container } from 'react-bootstrap';
+import './bootstrap.min.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome To Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
