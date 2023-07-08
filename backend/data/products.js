7895fa05ac967d31f57e9c70d5922ca12857e382
AtanasoaieD <danatanasoaie17@gmail.com>
2023-07-08 19:35:10 +0300

    create backend

---
+++ backend/data/products.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Commencal Meta tr ',
+    image: '/images/commencal-meta-tr.jpg',
+    description: 'Full suspension bike',
+    brand: 'Commencal',
+    price: '3500$',
+    countInStock: 3,
+    rating: 5.0,
+    numReviews: 4,
+  },
+
+  {
+    _id: '2',
+    name: 'Trek Marlin ',
+    image: '/images/marlin8.jpeg',
+    description: 'Hardtail',
+    brand: 'Trek',
+    price: '4000$',
+    countInStock: 4,
+    rating: 4.0,
+    numReviews: 3,
+  },
+
+  {
+    _id: '3',
+    name: 'Norco Range',
+    image: '/images/norco-range.jpg',
+    description: 'Full-Suspension',
+    brand: 'Norco',
+    price: '2000$',
+    countInStock: 2,
+    rating: 3.5,
+    numReviews: 2,
+  },
+  {
+    _id: '4',
+    name: 'NukeProof Scout',
+    image: '/images/np-scout-290.jpg',
+    description: 'HardTail',
+    brand: 'Nukeproof',
+    price: '1500$',
+    countInStock: 2,
+    rating: 2,
+    numReviews: 1,
+  },
+  {
+    _id: '5',
+    name: 'Trek Roscoe',
+    image: '/images/trek-roscoe-8.jpg',
+    description: 'HardTail',
+    brand: 'Trek',
+    price: '1800$',
+    countInStock: 6,
+    rating: 5,
+    numReviews: 6,
+  },
+];
+//commonjs
+module.exports = products;
+
+// react
+// export default products;
