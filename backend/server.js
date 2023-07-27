5b8e7fd144758f5879372c7c8b05b4bf43b2c0c1
AtanasoaieD <danatanasoaie17@gmail.com>
2023-07-27 17:58:30 +0300

    redux

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import products from './data/products.js';
 
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js';
 dotenv.config();
 //connect to db
 connectDB();
 
 const app = express();
+
+// app.use();
 app.get('/', (req, res) => {
   res.send('Api is running...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
 //get file based on id
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
 
 const PORT = process.env.PORT || 5000;
 
